e47a9e131d43c982fadef64faace51906f345c16
Leo Loosen <leo@getquin.com>
2020-10-31 15:12:31 +0100

    test

---
+++ index.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Die ist eine Test Website</h1>
+    <button class="button btn-outline-primary"></button>
+</template>
+
+<script>
+
+</script>
+
+<style lang="css">
+    h1 {
+        color: red;
+    }
+</style>
